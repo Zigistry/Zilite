c88b875767ef3a941cc004878246b0c146d913b2
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-09-09 02:50:55 +0530

    Refactor main functions into separate file

--- src/index.js
+++ src/index.js
@@ -1,45 +1,4 @@
-/**
- * 
- * @param {{regex: RegExp;className: string;}[]} language is the pattern input
- * @param {string} content is the code that needs to be highlighted input
- * @returns {string} is the output code.
- */
-export function highlight(language, content) {
-    content = content.replace(/ /g, "±±§§±±§§")
-        .replace(/=/g, "±§±§±§±§");
-    language.forEach(({ regex, className }) => {
-        content = content.replace(regex, (match) => `<span class="${className}">${match}</span>`);
-    });
-    content = content.replace(/±±§§±±§§/g, "&nbsp;")
-        .replace(/±§±§±§±§/g, "=")
-        .replace(/\n/g, "<br/>");
-    return content;
-}
-
-/**
- * 
- * @param {{regex: RegExp;className: string;}[]} language is the pattern input
- * @param {string} content is the code that needs to be highlighted input
- * @param {string} className is the black inside which the code is present.
- * @returns {void}
- */
-export function highlightElements(language, className) {
-    const codeContainers = document.getElementsByClassName(className);
-    for (let pre of codeContainers) {
-        const codeContent = pre.innerHTML;
-        const highlightedContent = highlight(language, codeContent);
-        pre.innerHTML = highlightedContent;
-    }
-}
-
-/**
- * 
- * @param {{regex: RegExp;className: string;}[]} language 
- * @param {string} id 
- */
-export function highlightElement(language, id) {
-    const codeContainer = document.getElementById(id);
-    const codeContent = codeContainer.innerHTML;
-    const highlightedContent = highlight(language, codeContent);
-    codeContainer.innerHTML = highlightedContent;
-}
+export * from "./language/bash"
+export * from "./language/zig"
+export * from "./language/diff"
+export * from "./main"
