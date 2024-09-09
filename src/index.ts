63b17056404abc017ed9b7bd7293a3ecf63f3357
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-09-09 16:15:05 +0530

    Update package version to 0.0.291

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { highlight, highlightElements, highlightElement } from "./main"
+export { highlight, highlightElements, highlightElement } from "./main.mjs"
 export { bash } from "./language/bash"
 export { zig } from "./language/zig"
 export { diff } from "./language/diff"
