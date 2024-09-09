5375643cabe9a48c2bc9c379e6dc0d63ed7af1f1
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-09-09 14:50:45 +0530

    Rename JavaScript files to .mjs

--- src/zilite.d.ts
+++ src/zilite.d.ts
@@ -7,7 +7,21 @@ interface LanguagePattern {
     regex: RegExp;
     className: string;
 }
-
+declare module 'zilite/src/language/zig.mjs' {
+    export declare const zig: LanguagePattern[];
+};
+declare module 'zilite/src/language/bash.mjs' {
+    export declare const bash: LanguagePattern[];
+};
+declare module 'zilite/src/main.mjs' {
+    export declare function highlight(language: LanguagePattern[], content: string): string;
+    export declare function highlightElements(language: LanguagePattern[], className: string): void;
+    export declare function highlightElement(language: LanguagePattern[], id: string): void;
+    export declare function zon2json(input: string): string;
+};
+declare module 'zilite/src/language/diff.mjs'{
+    export declare const diff: LanguagePattern[];
+};
 /**
  * Other function declarations for highlight, highlightElements, highlightElement, etc.
  */
