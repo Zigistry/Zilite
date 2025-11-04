df72e516800e240d30dc6d8ba49170d330a32615
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-11-04 11:27:31 +0530

    first release

--- tests/test.js
+++
@@ -1,5 +0,0 @@
-import * as hl from "higherlight";
-import { zig } from "higherlight/src/language/zig";
-
-const highlightedCode = hl.highlight(zig, "const x = 'asdf';");
-console.log(highlightedCode);
